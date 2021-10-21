b8d774a99d998fb87b75720481c254bbf85dbfb8
zhangjian <jian.zhang7@jiediankeji.com>
2021-10-21 13:21:21 +0800

    continue...

--- demo.py
+++ demo.py
@@ -1,3 +1,14 @@
-a = None
+import hashlib
 
-print(ord(a))
+username = 'zhangjian'
+password = b"\xa1\xcf\x0c\xf8'V\xf6\xcf\x05-\xf26\xbf\xcf\xa2\xfe"
+
+# 把用户名字节码当作盐加到对象中，加强密码的安全性
+# 返回一个新的类
+md5 = hashlib.md5(username.encode('utf-8'))
+# 添加要加密的字节码
+md5.update(password)
+# 获得密文
+content = md5.digest()
+
+print(content)
