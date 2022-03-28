638fa261dcf2fe138bc231bc23eb15ce0d102a3b
Seeker <Seeker@zhangjian.com>
2022-03-28 22:04:33 +0800

    新增 grpc_test 文件夹，演示grpc调用过程

---
+++ grpc_test/client.py
@@ -0,0 +1,19 @@
+import grpc
+
+from packages.greet_pb2 import helloRequest
+from packages.greet_pb2_grpc import GreetServiceStub
+
+
+def run():
+    # 使用上下文管理，自动关闭链接
+    with grpc.insecure_channel('localhost:50000') as channel:
+        # 客户端通过 stub 来实现 rpc 通信
+        stub = GreetServiceStub(channel)
+        # 调用服务端接口
+        res = stub.sayHello(helloRequest(name='zhangjian'))
+
+    print('server message:', res.msg)
+
+
+if __name__ == '__main__':
+    run()
---
+++ grpc_test/server.py
@@ -0,0 +1,36 @@
+import time
+from concurrent import futures
+
+import grpc
+
+from packages.greet_pb2 import helloResponse
+from packages.greet_pb2_grpc import add_GreetServiceServicer_to_server, GreetServiceServicer
+
+
+class Hello(GreetServiceServicer):
+    # 实现接口
+    def sayHello(self, request, context):
+        print(f'{request.name} is coming')
+        return helloResponse(msg='hello, %s' % request.name)
+
+
+def serve():
+    # 这里通过thread pool来并发处理server的任务
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    # 将对应的任务处理函数添加到rpc server中
+    add_GreetServiceServicer_to_server(Hello(), server)
+
+    # 这里使用的非安全接口，gRPC支持TLS/SSL安全连接，以及各种鉴权机制
+    server.add_insecure_port('[::]:50000')
+    server.start()
+
+    try:
+        while True:
+            time.sleep(60 * 60)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    serve()
