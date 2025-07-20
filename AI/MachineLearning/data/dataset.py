1d50ecc98b6812271c5e9e59d2f84a84b01865fc
Seeker <zj19180525254@163.com>
2025-07-20 17:28:37 +0800

    continue...

--- AI/MachineLearning/data/dataset.py
+++ AI/MachineLearning/data/dataset.py
@@ -1,6 +1,15 @@
+import os
+
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("johnsmith88/heart-disease-dataset")
+path = kagglehub.dataset_download("ruizema123/advertising-channel-analysis")
 
 print("Path to dataset files:", path)
+
+# 当前目录
+workdir = os.path.dirname(os.path.abspath(__file__)) + "/"
+
+os.system(f"mv {path} {workdir}")
+
+
--- AI/draft.py
+++ AI/draft.py
@@ -1,28 +1,49 @@
-from sklearn.datasets import load_iris
+import pandas as pd
+from pandas import Series
+from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler
 
-x, y = load_iris(return_X_y=True, as_frame=False)
+data = pd.read_csv("./data/ad_data_new.csv")
 
-# 数据降维  y本身就1维，就不做处理了
-# 把四维降到二维
-pca = PCA(n_components=2)
-x = pca.fit_transform(x)
-print(x.shape)  # (150, 2)
+print(data.shape)  # (889, 32)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=5)
+# 数据降维
+pca = PCA(n_components=3)
+data = pca.fit_transform(data)
 
-scaler = StandardScaler()
-scaler.fit(x_train)
+print(data.shape)  # (889, 3)
 
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
+# 用降维后的数据聚类
+model = KMeans(n_clusters=9, random_state=5)
+y_pred = model.fit_predict(data)
 
-# 使用sklearn模型
-model = KNeighborsClassifier(n_neighbors=7)
-model.fit(x_train, y_train)
+# 基于原始数据构造分析数据
+data = pd.read_csv("./data/ad_data.csv")
+data = pd.concat((data.iloc[:, 2:], pd.DataFrame(y_pred, columns=["clusters"])), axis=1)
 
-score = model.score(x_test, y_test)
-print(score)  # 0.9666666666666667
+# 数据分析
+features = []
+for i in range(9):
+    group = data[data["clusters"] == i]
+
+    # 数值部分
+    nums = group.iloc[:, :7].describe().round(3)
+    # 平均值在第二行，我们就去 第二行的所有列
+    mean = nums.iloc[1, :]
+
+    # 字符串部分
+    strs = group.iloc[:, 7:-1].describe(include="all")
+    # top 值在第三行
+    top = strs.iloc[2, :]
+
+    # 把当前分类标签放到第一列，类别样本总数放第二列，分类占比放第三列
+    base = Series([i, group["clusters"].count(), (group["clusters"].count() / data["clusters"].count()).round(3)],
+                  index=["clusters", "count", "ratio"])
+
+    # 把分析数据拼到一起
+    feature: Series = pd.concat((base, mean, top), axis=0)
+
+    features.append(feature)
+
+# 把分析数据保存到文件
+pd.DataFrame(features).to_csv("./data/da_data_analysis_pca.csv", index=False)
